f4c10ed7748f93fb2b1dbc309f5bb4d7ed215436
David Mendez Guardado <demg@outlook.com>
2018-12-03 13:23:30 -0600

    1) Iris Flowers Classification ML Project– Learn about Supervised Machine Learning Algorithms Iris flowers dataset is one of the best data sets in classification literature. The classification of the iris flowers machine learning project is often referred to as the “Hello World” of machine learning. Furthermore, this dataset has numeric attributes and beginners need to figure out how to load and handle data. Also, the iris dataset is small which easily fits into the memory and does not require any special transformations or scaling, to begin with.
    Iris Dataset can be downloaded from UCI ML Repository — Download Iris Flowers Dataset
    
    The goal of this machine learning project is to classify the flowers into among the three species — virginica, setosa, or versicolor based on length and width of petals and sepals.

--- IrisFlowersClassification/Program.cs
+++ IrisFlowersClassification/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.ML.Legacy;
+using Microsoft.ML.Legacy.Data;
+using Microsoft.ML.Legacy.Trainers;
+using Microsoft.ML.Legacy.Transforms;
 
 namespace IrisFlowersClassification
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static readonly string _dataPath = Path.Combine(Environment.CurrentDirectory, "Data", "iris.data");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "IrisClusteringModel.zip");
+
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PredictionModel<IrisData, ClusterPrediction> model = Train();
+            await model.WriteAsync(_modelPath);
+            var prediction = model.Predict(TestIrisData.Setosa);
+            Console.WriteLine($"Cluster: {prediction.PredictedClusterId}");
+            Console.WriteLine($"Distances: {string.Join(" ", prediction.Distances)}");
+            Console.Write("press any key to close...");
+            Console.ReadKey();
+        }
+
+        private static PredictionModel<IrisData, ClusterPrediction> Train()
+        {
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader(_dataPath).CreateFrom<IrisData>(separator: ','));
+            pipeline.Add(new ColumnConcatenator(
+                    "Features",
+                    "SepalLength",
+                    "SepalWidth",
+                    "PetalLength",
+                    "PetalWidth"));
+            pipeline.Add(new KMeansPlusPlusClusterer() { K = 3 });
+            var model = pipeline.Train<IrisData, ClusterPrediction>();
+            return model;
+
         }
     }
 }
